de4eeff240a573a58d9e594de689207c2c95af77
NebilJan <96645581+NebilJan@users.noreply.github.com>
2021-12-26 20:08:44 +0330

    update App.py

--- App.py
+++ App.py
@@ -8,6 +8,91 @@ def do():
      f=open("log.txt","a")
      f.write(str(request.form))
      f.close()
+     html="""
+     <!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv='content=text/html; charset=utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1.0'>
+        <title>Information Sender </title>
+        <style type='text/css' media='all'>
+        *{
+            margin: 0;
+            padding: 0;
+            outline: none;
+        }
+        body{
+            text-align: center;
+        }
+        table{
+            text-align: center;
+        }
+        table,td,th{
+            overflow: hidden;
+            overflow-x: auto;
+            padding: 8px;
+            
+            background: dodgerblue;
+            border-collapse: collapse;
+            border:1px solid#c1eef9;
+            color: white;
+        }
+        th{
+            color: gold;
+        }
+            td{
+                padding: 15px;
+            }
+            h3{
+                color: #067996;
+                padding: 15px;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <table border='0' width='100%'>
+            <tr>
+                <h3>
+                Request Message From Client
+                </h3>
+            </tr>
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Date
+                </th>
+                <th>Name</th>
+                    <th>
+                        Message
+                    </th>
+                    
+            </tr>
+            <tr>
+                <td>
+                    1
+                </td>
+                <td>
+                    201
+                </td>
+                <td>
+                    nebil
+                </td>
+                <td>
+                    Hi 
+                </td>
+            </tr>
+            
+        </table>
+    </body>
+</html>
+     
+     
+     
+     
+     """
      return f"Hi {user}  <br> thanx you msg: {msg}"
 if __name__ == "__main__":
     App.run("0.0.0.0",5000,debug=True)
